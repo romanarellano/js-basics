852d58ee2bfc4cefd5c658f1fa8f88cce3a36197
romanarellano <romanarellano@yahoo.com>
2015-01-13 13:48:47 -1000

    first exercise

--- script.js
+++ script.js
@@ -1,24 +1,48 @@
 /* Create a `name` variable and assign it a String value */
-
+var name= "Roman";
 /* Create a `person` variable and give it 2 properties,
  * `name`, assign it the same name as before,
  * as well as an `age` (number);
  */
+var person={
 
+  name:name,
+age:54
+}
 /* Create a function called `greet`,
  * it should take a 1 parameter, `name`
  * and it should print "Hello, my name is {name}"
  */
+// function greet(name){
 
+// console.log("Hello, my name is " + name)
+
+// }
 /* Create a variable called `canDrive`,
  * if it should be true if your person object is at least 16 years old
  */
+var canDrive= person.age>=16
+  
 
+function greet(name){
+
+console.log("Hello, my name is " + name)
+
+}
 /* Create an array called `dataTypes` with atleast 1 of every data type;
  * (there are 6 different data types);
  */
+var dataTypes= [22,"hello",[434,"343"],{yup:"alright"},true,undefined,null]
+
 
 /* Create a Dog object using the `new` constructor.
  * it should have a `bark` function that makes your dog bark!
  * It should also have a name attribute.
  */
+
+ // var Dog= new Object();
+ // Dog.bark= function (){
+
+ //  console.log("bark")
+ // }
+ // Dog.name="Mimi";
